200f0bd17e0cd036bd1a1fab78780ab6dc23e5da
tosin <tosin.sonuyi@gmail.com>
2019-03-15 01:36:08 -0400

    adds setup.py for tox

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='lambda-webscrape',
+      version='0.1',
+      description='The webscrapinglambda',
+      author='and-computers',
+      author_email='jcp@mail.andcomputers.io',
+      license='MIT')

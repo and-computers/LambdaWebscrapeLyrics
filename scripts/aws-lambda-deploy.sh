d6ecd5ca75780cfbd9e2aa6e2ee2226b961a7dd4
tosin <tosin.sonuyi@gmail.com>
2019-03-16 15:30:07 -0400

    fix attempt on deploy package dir structure

--- scripts/aws-lambda-deploy.sh
+++ scripts/aws-lambda-deploy.sh
@@ -4,25 +4,28 @@ echo "beginning deployment script"
 
 echo "install requirements.txt"
 mkdir lambda_deployment_package
-pip install --no-deps -r requirements.txt --target lambda_deployment_package
+cd lambda_deployment_package
+pip install --no-deps -r requirements.txt --target .
 echo "copying lambda function to deployment package"
 #cp  lambda_handler.py lambda_deployment_package/
 
 
-echo "removing provided AWS libraries, botocore and boto3"
+# echo "removing provided AWS libraries, botocore and boto3"
 
-rm -r lambda_deployment_package/boto3* 
-rm -r lambda_deployment_package/botocore*
-rm -r lambda_deployment_package/tox*
+# rm -r lambda_deployment_package/boto3* 
+# rm -r lambda_deployment_package/botocore*
+# rm -r lambda_deployment_package/tox*
 
 
 echo "zipping deployment package"
-zip -r9 lambda_deployment_package.zip lambda_deployment_package
-zip -g lambda_deployment_package.zip lambda_handler.py
+zip -r9 ../function.zip .
+# zip -r9 lambda_deployment_package.zip lambda_deployment_package
+cd ../
+zip -g function.zip lambda_handler.py
 
 
-echo "removing package folder"
-rm -r lambda_deployment_package
+# echo "removing package folder"
+# rm -r lambda_deployment_package
 
 ACCESS_KEY=$1
 SECRET_KEY=$2
@@ -33,7 +36,7 @@ AWS_ACCESS_KEY_ID=$ACCESS_KEY AWS_SECRET_ACCESS_KEY=$SECRET_KEY AWS_DEFAULT_REGI
 	--handler "lambda_handler.handler" \
 	--timeout 120 \
 	--memory-size 256 \
-	--zip-file fileb://lambda_deployment_package.zip \
+	--zip-file fileb://function.zip \
 	--runtime python3.6 \
 	--description "lambda for scheduling webscrape lyrics" \
 	--environment "Variables={TestVar=TestValue,TestVar2=TestValue2}" \

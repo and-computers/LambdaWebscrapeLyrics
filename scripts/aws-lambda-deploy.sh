089fc1358c73a4411f38dd323fef776241bc482d
tosin <tosin.sonuyi@gmail.com>
2019-03-16 15:33:58 -0400

    move requriements install command

--- scripts/aws-lambda-deploy.sh
+++ scripts/aws-lambda-deploy.sh
@@ -5,7 +5,7 @@ echo "beginning deployment script"
 echo "install requirements.txt"
 mkdir lambda_deployment_package
 cd lambda_deployment_package
-pip install --no-deps -r requirements.txt --target .
+pip install --no-deps -r ../requirements.txt --target .
 echo "copying lambda function to deployment package"
 #cp  lambda_handler.py lambda_deployment_package/
 

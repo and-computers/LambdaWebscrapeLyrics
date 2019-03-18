3382ba6c8c56e39effef89d6ddce9487cbc5fbd2
tosin <tosin.sonuyi@gmail.com>
2019-03-18 00:23:38 -0400

    readjust spacing/indentation

--- scripts/deploy_orchestration.py
+++ scripts/deploy_orchestration.py
@@ -16,9 +16,9 @@ cfg.read('deployment_config.ini')
 # parse lambda configuration parameters
 CONFIG_PARAMS = {}
 for config_tuple in cfg.items('configuration'):
-	name = config_tuple[0]
-	val = config_tuple[1]
-	CONFIG_PARAMS[name] = val
+    name = config_tuple[0]
+    val = config_tuple[1]
+    CONFIG_PARAMS[name] = val
 
 
 # https://stackoverflow.com/questions/5466451/how-can-i-print-literal-curly-brace-characters-in-python-string-and-also-use-fo#5466478
@@ -32,9 +32,9 @@ logging.info("Parsed environment variables:\n{}".format(VARS_STR))
 
 
 def is_func_new(funcname):
-	"""
-	determine if function being deployed is brand new or just needs updates
-	"""
+    """
+    determine if function being deployed is brand new or just needs updates
+    """
     bashCommand = "aws lambda list-functions"
     process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
@@ -44,79 +44,76 @@ def is_func_new(funcname):
             return False
     return True
 
+
 def deploy_brand_new_lambda():
-	"""
-	ACCESS_KEY=$1
-	SECRET_KEY=$2
-	RESOURCE_ROLE=$3
+    """
+    ACCESS_KEY=$1
+    SECRET_KEY=$2
+    RESOURCE_ROLE=$3
 
-	ISNEW=$4
+    ISNEW=$4
 
-	FNAME=$5
-	HANDLER=$6
-	TIMEOUT=$7
-	MEMSIZE=$8
-	DESC=$9
-	ENV=${10}
-	RUNTIME=${11}
-	REGION=${12}
-	"""
+    FNAME=$5
+    HANDLER=$6
+    TIMEOUT=$7
+    MEMSIZE=$8
+    DESC=$9
+    ENV=${10}
+    RUNTIME=${11}
+    REGION=${12}
+    """
 
-	bashCommand = "bash scripts/aws-lambda-deploy.sh \
-	{AWS_ACCESS_KEY} \
-	{AWS_SECRET_KEY} \
-	{AWS_LAMBDA_ROLE} \
-	{isnew} \
-	{funcname} {handler} {timeout} {memsize} {desc} {env} {runtime} {region}".format(
-		AWS_ACCESS_KEY=ACCESS_KEY,
-		AWS_SECRET_KEY=SECRET_KEY,
-		AWS_LAMBDA_ROLE=RESOURCE_ROLE,
-		isnew='yes',
-		funcname=CONFIG_PARAMS['function-name'],
-		desc=CONFIG_PARAMS['description'],
-		runtime=CONFIG_PARAMS['runtime'],
-		handler=CONFIG_PARAMS['handler'],
-		region=CONFIG_PARAMS['region'],
-		timeout=CONFIG_PARAMS['timeout'],
-		mem=CONFIG_PARAMS['memory-size'],
-		env=VARS_STR
-		)
+    bashCommand = "bash scripts/aws-lambda-deploy.sh \
+    {AWS_ACCESS_KEY} \
+    {AWS_SECRET_KEY} \
+    {AWS_LAMBDA_ROLE} \
+    {isnew} \
+    {funcname} {handler} {timeout} {memsize} {desc} {env} {runtime} {region}".format(
+        AWS_ACCESS_KEY=ACCESS_KEY,
+        AWS_SECRET_KEY=SECRET_KEY,
+        AWS_LAMBDA_ROLE=RESOURCE_ROLE,
+        isnew='yes',
+        funcname=CONFIG_PARAMS['function-name'],
+        desc=CONFIG_PARAMS['description'],
+        runtime=CONFIG_PARAMS['runtime'],
+        handler=CONFIG_PARAMS['handler'],
+        region=CONFIG_PARAMS['region'],
+        timeout=CONFIG_PARAMS['timeout'],
+        mem=CONFIG_PARAMS['memory-size'],
+        env=VARS_STR
+    )
 
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
 
 
 def update_existing_lambda():
-	bashCommand = "bash scripts/aws-lambda-deploy.sh \
-	{AWS_ACCESS_KEY} \
-	{AWS_SECRET_KEY} \
-	{AWS_LAMBDA_ROLE} \
-	{isnew} \
-	{funcname} {handler} {timeout} {memsize} {desc} {env} {runtime} {region}".format(
-		AWS_ACCESS_KEY=ACCESS_KEY,
-		AWS_SECRET_KEY=SECRET_KEY,
-		AWS_LAMBDA_ROLE=RESOURCE_ROLE,
-		isnew='no',
-		funcname=CONFIG_PARAMS['function-name'],
-		desc=CONFIG_PARAMS['description'],
-		runtime=CONFIG_PARAMS['runtime'],
-		handler=CONFIG_PARAMS['handler'],
-		region=CONFIG_PARAMS['region'],
-		timeout=CONFIG_PARAMS['timeout'],
-		mem=CONFIG_PARAMS['memory-size'],
-		env=VARS_STR
-		)
+    bashCommand = "bash scripts/aws-lambda-deploy.sh \
+    {AWS_ACCESS_KEY} \
+    {AWS_SECRET_KEY} \
+    {AWS_LAMBDA_ROLE} \
+    {isnew} \
+    {funcname} {handler} {timeout} {memsize} {desc} {env} {runtime} {region}".format(
+        AWS_ACCESS_KEY=ACCESS_KEY,
+        AWS_SECRET_KEY=SECRET_KEY,
+        AWS_LAMBDA_ROLE=RESOURCE_ROLE,
+        isnew='no',
+        funcname=CONFIG_PARAMS['function-name'],
+        desc=CONFIG_PARAMS['description'],
+        runtime=CONFIG_PARAMS['runtime'],
+        handler=CONFIG_PARAMS['handler'],
+        region=CONFIG_PARAMS['region'],
+        timeout=CONFIG_PARAMS['timeout'],
+        mem=CONFIG_PARAMS['memory-size'],
+        env=VARS_STR
+    )
 
-	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-	output, error = process.communicate()
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
 
 if is_func_new():
-	logging.info("Deploying New Function")
-	deploy_brand_new_lambda()
+    logging.info("Deploying New Function")
+    deploy_brand_new_lambda()
 else:
-	logging.info("Updating Existing Function")
-	update_existing_lambda()
-
-
-
-
+    logging.info("Updating Existing Function")
+    update_existing_lambda()
